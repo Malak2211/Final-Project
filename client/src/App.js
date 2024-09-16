c689fad1e7e3729a67345db246dd873c699216fa
Miral <miralalamir20@gmail.com>
2024-09-16 08:08:38 +0300

    homepage not completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Login from './Items/Login';
 import Signup from './Items/Signup';
-import Home from './Items/home'
+import Home from './Items/home';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './Items/designforlogin.css';
 import './Items/designforsignup.css';
+import './Items/designforhome.css';
 
 function App() {
   return (
     <div className="App"> 
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Signup />}/>
+          <Route path='/' element={<Home />}/>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
+          {/* <Route path="/home" element={<Home />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
