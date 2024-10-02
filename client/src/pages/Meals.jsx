e0b098b00ba6068d1e2930c58ae1605bc58c9d9f
Hassan-Tamer <hassantamerha@gmail.com>
2024-10-02 17:41:36 +0300

    fix meals enter btn

--- client/src/pages/Meals.jsx
+++ client/src/pages/Meals.jsx
@@ -1,22 +1,15 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../pages/Meals.css'; 
-import axios from "axios";
 import { useNavigate } from 'react-router-dom'; 
 import {useRef} from "react";
 
-function text() {
-  return '[{"name": "John","age": 30,"city": "New York"},{"name": "Jane","age": 25,"city": "San Francisco"},{"name": "Mike","age": 35,"city": "Chicago"}]'
-}
-
 function Meals() {
-  const [choose, setChoose] = useState('');
-  const [search, setSearch] = useState('');
   const [meals, setMeals] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const dummyNum = useRef(0);
 
   // let meals = [];
   const newName = useRef("");
-  const dummyNum = useRef(0);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -24,6 +17,14 @@ function Meals() {
     console.log(inputValue); 
     getResponse(inputValue)
   };
+
+  useEffect(() => {
+    if (dummyNum.current === 0) {
+      getResponse('all countries');
+      console.log('test');
+    }
+    dummyNum.current = 5;
+  }, []);
   
   const filter = async (value) => {
         newName.current = value;
@@ -45,8 +46,28 @@ function Meals() {
             console.error('Error parsing JSON:', jsonError);  
           }
   };
+  
+  function setText(rawText) {
+    try {
+      const data = JSON.parse(rawText);
+      console.log('Parsed JSON:', data);
+      const data2 = JSON.parse(data);
+      console.log(data2)
+      setMeals(data2); 
+      return data2;
+    } catch (jsonError) {
+      console.error('Error parsing JSON:', jsonError);  
+    }
+  }
 
   const getResponse = async (country) => {
+    const cachedMeals = localStorage.getItem(country);
+    if (cachedMeals) {
+      let text_input = JSON.parse(cachedMeals);
+      setMeals(text_input); 
+      return;
+    }
+
     try {
       const response = await fetch('http://localhost:8080/generate', {
         method: 'POST',
@@ -59,45 +80,35 @@ function Meals() {
       });
   
       const rawText = await response.text();
-      try {
-        const data = JSON.parse(rawText);
-        console.log('Parsed JSON:', data);
-        const data2 = JSON.parse(data);
-        console.log(data2)
-        setMeals(data2); 
-      } catch (jsonError) {
-        console.error('Error parsing JSON:', jsonError);  
-      }
+      let parsedData = setText(rawText);
+      localStorage.setItem(country, JSON.stringify(parsedData));
+      
     } catch (error) {
       console.error('Error fetching or parsing JSON:', error);  
     }
   };
 
-  useEffect(() => {
-    if (dummyNum.current === 0) {
-      getResponse('all countries');
-      console.log('test');
-    }
-    dummyNum.current = 5;
-  }, []);
-
   
   return (
     <div>
       {/* <button onClick={getResponse}>Fetch Meals</button> */}
-      
+
       <form onSubmit={handleSubmit} className="meals_inputs">
-        <div className="back1">
-      <div className="meal_input"  style={{alignItems: "center", justifyContent: 'center'}}>
+      <div className="meal_input" style={{alignItems: "center", justifyContent: 'center'}}>
         <input 
           type="text" 
+          className='meal'
           placeholder="Enter Country Name" 
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter")
+              setInputValue(e.target.value);
+          }}
         />
-        <button type="submit">Submit</button>
+        
       </div>
-      </div>
+      <button className='searching_button' type="submit">Submit</button>
     </form>
 
 
@@ -105,7 +116,7 @@ function Meals() {
 
       <div className="meal_container">
         {meals.length === 0 ?(
-          <p>No meals available. Click the button to fetch meals.</p>
+          <p8>No meals available. Click the button to fetch meals.</p8>
         ) : (
           meals.map((item, index) => (
             <div className='meal_card' key={index}>
